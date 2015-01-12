33364ade4fcd99691b24af2de721ecac4782bf91
狼骑兵 <wuweifeng@747.cn>
2015-01-12 18:43:27 +0800

    删除无用东西

--- CommunityNews/src/com/tianyalei/communitynews/MainActivity.java
+++ CommunityNews/src/com/tianyalei/communitynews/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.activity_main);
 
         mContext = this;
 
