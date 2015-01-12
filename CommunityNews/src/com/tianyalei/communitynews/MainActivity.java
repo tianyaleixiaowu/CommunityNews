9c4c847f7d8a835bea03d42c91421b72476dc66f
Paul <xuyihan@747.cn>
2015-01-12 18:29:25 +0800

    添加首页图片轮播

--- CommunityNews/src/com/tianyalei/communitynews/MainActivity.java
+++ CommunityNews/src/com/tianyalei/communitynews/MainActivity.java
@@ -8,27 +8,129 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.view.ViewPager;
 import android.view.KeyEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import com.baidu.android.pushservice.CustomPushNotificationBuilder;
 import com.baidu.android.pushservice.PushConstants;
 import com.baidu.android.pushservice.PushManager;
+import com.lidroid.xutils.ViewUtils;
+import com.lidroid.xutils.view.annotation.ViewInject;
+import com.tianyalei.communitynews.adapter.ImageViewPagerAdapter;
 import com.tianyalei.communitynews.utils.Utils;
 import com.umeng.analytics.MobclickAgent;
 
-public class MainActivity extends Activity {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
+public class MainActivity extends Activity {
+    /**
+     * ViewPager
+     */
+    @ViewInject(R.id.vp)
+    private ViewPager mViewPager;
+    /**
+     * 轮播图片集合
+     */
+    private List<ImageView> mImageViewList;
+    /**
+     * 标题集合
+     */
+    private String[] titles;
+    /**
+     * 图片ID集合
+     */
+    private int[] imageResId;
+    /**
+     * 标题textview
+     */
+    @ViewInject(R.id.tv_title)
+    private TextView mTvTitle;
+    /**
+     * 当前图片索引
+     */
+    private int currentItem = 0;
+    /**
+     * 我也不知道是啥....
+     */
+    private ScheduledExecutorService mScheduledExecutorService;
+    /**
+     * 小点
+     */
+    private List<View> dots;
     private Context mContext;
+    // 切换当前显示的图片
+    private Handler handler = new Handler() {
+        public void handleMessage(android.os.Message msg) {
+            mViewPager.setCurrentItem(currentItem);// 切换当前显示的图片
+        }
+    };
+
     /**
      * Called when the activity is first created.
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-
+        setContentView(R.layout.activity_main);
+        ViewUtils.inject(this);
         mContext = this;
-
         initPushService();
+        init();
+    }
+
+    /**
+     * 初始化
+     */
+    private void init() {
+        imageResId = new int[]{R.drawable.a, R.drawable.b, R.drawable.c, R.drawable.d, R.drawable.e};
+        titles = new String[imageResId.length];
+        titles[0] = "巩俐不低俗，我就不能低俗";
+        titles[1] = "扑树又回来啦！再唱经典老歌引万人大合唱";
+        titles[2] = "揭秘北京电影如何升级";
+        titles[3] = "乐视网TV版大派送";
+        titles[4] = "热血屌丝的反杀";
+
+        mImageViewList = new ArrayList<ImageView>();
+
+        // 初始化图片资源
+        for (int i = 0; i < imageResId.length; i++) {
+            ImageView imageView = new ImageView(this);
+            imageView.setImageResource(imageResId[i]);
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            mImageViewList.add(imageView);
+        }
+        dots = new ArrayList<View>();
+        dots.add(findViewById(R.id.v_dot0));
+        dots.add(findViewById(R.id.v_dot1));
+        dots.add(findViewById(R.id.v_dot2));
+        dots.add(findViewById(R.id.v_dot3));
+        dots.add(findViewById(R.id.v_dot4));
+        mTvTitle.setText(titles[0]);//
+        mViewPager.setAdapter(new ImageViewPagerAdapter(mImageViewList));// 设置填充ViewPager页面的适配器
+        // 设置一个监听器，当ViewPager中的页面改变时调用
+        mViewPager.setOnPageChangeListener(new MyPageChangeListener());
+    }
+
+    @Override
+    protected void onStart() {
+        mScheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+        // 当Activity显示出来后，每两秒钟切换一次图片显示
+        mScheduledExecutorService.scheduleAtFixedRate(new ScrollTask(), 1, 2, TimeUnit.SECONDS);
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        // 当Activity不可见的时候停止切换
+        mScheduledExecutorService.shutdown();
+        super.onStop();
     }
 
     private void initPushService() {
@@ -126,4 +228,48 @@ public class MainActivity extends Activity {
                 });
         builder.show();
     }
+
+    /**
+     * 执行切换任务
+     */
+    private class ScrollTask implements Runnable {
+        public void run() {
+            synchronized (mViewPager) {
+                System.out.println("currentItem: " + currentItem);
+                currentItem = (currentItem + 1) % mImageViewList.size();
+                handler.obtainMessage().sendToTarget(); // 通过Handler切换图片
+            }
+        }
+    }
+
+    /**
+     * 当ViewPager中页面的状态发生改变时调用
+     *
+     * @author Administrator
+     */
+    private class MyPageChangeListener implements ViewPager.OnPageChangeListener {
+        private int oldPosition = 0;
+
+        /**
+         * This method will be invoked when a new page becomes selected.
+         * position: Position index of the new selected page.
+         */
+        public void onPageSelected(int position) {
+            currentItem = position;
+            mTvTitle.setText(titles[position]);
+            dots.get(oldPosition).setBackgroundResource(R.drawable.dot_normal);
+            dots.get(position).setBackgroundResource(R.drawable.dot_focused);
+            oldPosition = position;
+        }
+
+        public void onPageScrollStateChanged(int arg0) {
+
+        }
+
+        public void onPageScrolled(int arg0, float arg1, int arg2) {
+
+        }
+    }
+
+
 }
