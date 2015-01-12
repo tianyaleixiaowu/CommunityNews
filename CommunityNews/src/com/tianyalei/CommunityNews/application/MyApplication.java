3f24a8c53b997b3da337f8b4dfbe74ff74706475
狼骑兵 <wuweifeng@747.cn>
2015-01-12 14:21:02 +0800

    1

---
+++ CommunityNews/src/com/tianyalei/CommunityNews/application/MyApplication.java
@@ -0,0 +1,24 @@
+package com.tianyalei.CommunityNews.application;
+
+import com.baidu.frontia.FrontiaApplication;
+
+/**
+ * Created by wuwf on 2015/1/12.
+ */
+public class MyApplication extends FrontiaApplication {
+
+    private static MyApplication context;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        context = this;
+
+    }
+
+    public static MyApplication getInstance() {
+        return context;
+    }
+
+}
