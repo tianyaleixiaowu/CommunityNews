434de348b98108489d8660ce1131bdd159961a3b
狼骑兵 <wuweifeng@747.cn>
2015-01-13 18:08:43 +0800

    1

---
+++ CommunityNews/src/com/tianyalei/communitynews/adapter/MainFragmentPagerAdapter.java
@@ -0,0 +1,63 @@
+package com.tianyalei.communitynews.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import com.ab.fragment.AbFragment;
+import com.shizhefei.view.indicator.IndicatorViewPager;
+import com.tianyalei.communitynews.R;
+import com.tianyalei.communitynews.application.MyApplication;
+import com.tianyalei.communitynews.fragment.MainFirstFragment;
+
+/**
+ * Created by wuwf on 2015/1/13.
+ */
+public class MainFragmentPagerAdapter extends IndicatorViewPager.IndicatorFragmentPagerAdapter {
+    private String[] tabNames = {"主页", "消息", "发现", "我"};
+    private int[] tabIcons = {R.drawable.maintab_1_selector, R.drawable.maintab_2_selector, R.drawable.maintab_3_selector,
+            R.drawable.maintab_4_selector};
+    private LayoutInflater inflater;
+
+    public MainFragmentPagerAdapter(FragmentManager fragmentManager) {
+        super(fragmentManager);
+        inflater = LayoutInflater.from(MyApplication.getInstance());
+    }
+
+    @Override
+    public int getCount() {
+        return tabNames.length;
+    }
+
+    @Override
+    public View getViewForTab(int position, View convertView, ViewGroup container) {
+        if (convertView == null) {
+            convertView = (TextView) inflater.inflate(R.layout.tab_bottom_text, container, false);
+        }
+        TextView textView = (TextView) convertView;
+        textView.setText(tabNames[position]);
+        textView.setCompoundDrawablesWithIntrinsicBounds(0, tabIcons[position], 0, 0);
+        return textView;
+    }
+
+    @Override
+    public Fragment getFragmentForPage(int position) {
+        AbFragment fragment = null;
+        switch (position) {
+            case 0:
+                fragment = new MainFirstFragment();
+
+                break;
+            default:
+                break;
+        }
+//        MainFragment mainFragment = new MainFragment();
+//        Bundle bundle = new Bundle();
+//        bundle.putString(MainFragment.INTENT_STRING_TABNAME, tabNames[position]);
+//        bundle.putInt(MainFragment.INTENT_INT_INDEX, position);
+//        mainFragment.setArguments(bundle);
+        return fragment;
+    }
+}
